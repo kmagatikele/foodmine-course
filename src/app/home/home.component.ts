b49ddab9cbcd47f570e1287887433c7d9a1a9f8b
Keletso Moselakgomo <Keletso.Moselakgomo@mixtelematics.com>
2023-11-07 08:40:28 +0200

    Part 7 - Adding Search Bar - Angular Part

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FoodService } from '../services/food/food.service';
+import { Food } from '../shared/models/food';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,11 +9,16 @@ import { FoodService } from '../services/food/food.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit{
-  foods:String[] = [];
+  foods:Food[] = [];
 
-  constructor(private foodService:FoodService){}
+  constructor(private foodService:FoodService, private route:ActivatedRoute){}
 
   ngOnInit(): void {
-    this.foods = this.foodService.getAll();
+    this.route.params.subscribe(params => {
+      if(params['searchTerm'])
+        this.foods = this.foodService.getAll().filter(food => food.name.toLowerCase().includes(params['searchTerm'].toLowerCase));
+      else
+        this.foods = this.foodService.getAll();
+    }) 
   }
 }
